8ac051d9f808dc8df2f6c9ad4dd6158609a246dd
IAmQpac <77878877+IAmQpac@users.noreply.github.com>
2022-07-16 17:59:51 -0400

    added description

--- twitterClient.js
+++ twitterClient.js
@@ -18,29 +18,39 @@ const tweet = async ()=>{
         let res = await fetch("https://api.tickertick.com/feed?q=(and (or tt:amzn tt:aapl tt:nflx tt:meta tt:goog tt:msft) (or T:fin_news T:earnings T:market T:sec T:sec_fin))")
 
         let tickerTickJSON = await res.json()
+        
         let title = tickerTickJSON.stories[0].title
         let description = tickerTickJSON.stories[0].description
+        description = "\nDesc: " + description
         let url = tickerTickJSON.stories[0].url
 
-        description.slice(0, 150)
-        description += "..."
-
+        let remainingChar = 280 - title.length - url.length
+        
         let tags;
         let tagString;
         tags = tickerTickJSON.stories[0].tags
         for (let i = 0; i < tags.length; i++) {
-            tagString += `$${tags[i].toUpperCase()} `                 
+            tagString += `$${tags[i].toUpperCase()} `     
+            remainingChar -= tagString.length           
+        }
+
+        remainingChar -= 10 //a small buffer for the spaces
+
+        if(remainingChar > 20) {
+            description.slice(0, remainingChar)
+        }else{
+            description = ""
         }
 
         if(!latestPostedStoryID){
             console.log("We have a new Story. Posting.. ", title)
-            T.post('statuses/update', {status: `${title}\nDesc: ${description}\n${tagString}\nPowered by TickerTick.com ${url}` }, function (err, data, response) {
+            T.post('statuses/update', {status: `${title}${description}\n${tagString}\nPowered by TickerTick.com ${url}` }, function (err, data, response) {
                 console.log(data)
             })
             latestPostedStoryID = tickerTickJSON.stories[0].id
         } else if(latestPostedStoryID != tickerTickJSON.stories[0].id){
             console.log("We have a new Story. Posting.. ", title)
-            T.post('statuses/update', {status: `${title}\nDesc: ${description}\n${tagString}\nPowered by TickerTick.com ${url}` }, function (err, data, response) {
+            T.post('statuses/update', {status: `${title}${description}\n${tagString}\nPowered by TickerTick.com ${url}` }, function (err, data, response) {
                 console.log(data)
             })
             latestPostedStoryID = tickerTickJSON.stories[0].id
