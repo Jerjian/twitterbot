c02ca5ad741b3a7c5f1a385a993d6106dd86a50f
IAmQpac <77878877+IAmQpac@users.noreply.github.com>
2022-07-16 17:19:44 -0400

    added description

--- twitterClient.js
+++ twitterClient.js
@@ -15,7 +15,7 @@ const tweet = async ()=>{
     try{
         //get 50 of the latest META news in the last hour 
         console.log("Fetching...")
-        let res = await fetch("https://api.tickertick.com/feed?q=(and (or tt:amzn tt:aapl tt:nflx tt:meta tt:goog tt:msft ) (or T:fin_news T:earnings T:market T:sec T:sec_fin))")
+        let res = await fetch("https://api.tickertick.com/feed?q=(and (or tt:amzn tt:aapl tt:nflx tt:meta tt:goog tt:msft) (or T:fin_news T:earnings T:market T:sec T:sec_fin))")
 
         let tickerTickJSON = await res.json()
 
@@ -25,22 +25,20 @@ const tweet = async ()=>{
         if(!latestPostedStoryID){
             console.log("We have a new Story. Posting.. ", tickerTickJSON.stories[0].title)
             tags = tickerTickJSON.stories[0].tags
-            tagString = "Tags: ";
             for (let i = 0; i < tags.length; i++) {
-                tagString += `$${tags[i]} `                 
+                tagString += `$${tags[i].toUpperCase()} `                 
             }
-            T.post('statuses/update', {status: `${tickerTickJSON.stories[0].title}\n${tagString}\nPowered by TickerTick.com ${tickerTickJSON.stories[0].url}` }, function (err, data, response) {
+            T.post('statuses/update', {status: `${tickerTickJSON.stories[0].title}\nDesc: ${tickerTickJSON.stories[0].description}\n${tagString}\nPowered by TickerTick.com ${tickerTickJSON.stories[0].url}` }, function (err, data, response) {
                 console.log(data)
             })
             latestPostedStoryID = tickerTickJSON.stories[0].id
         } else if(latestPostedStoryID != tickerTickJSON.stories[0].id){
             tags = tickerTickJSON.stories[0].tags
-            tagString = "Tags: ";
             for (let i = 0; i < tags.length; i++) {
                 tagString += `$${tags[i]} `                 
             }
             console.log("We have a new Story. Posting.. ", tickerTickJSON.stories[0].title)
-            T.post('statuses/update', {status: `${tickerTickJSON.stories[0].title}\n${tagString}\nPowered by TickerTick.com ${tickerTickJSON.stories[0].url}` }, function (err, data, response) {
+            T.post('statuses/update', {status: `${tickerTickJSON.stories[0].title}\nDesc: ${tickerTickJSON.stories[0].description}\n${tagString}\nPowered by TickerTick.com ${tickerTickJSON.stories[0].url}` }, function (err, data, response) {
                 console.log(data)
             })
             latestPostedStoryID = tickerTickJSON.stories[0].id
