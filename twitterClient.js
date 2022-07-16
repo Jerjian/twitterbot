7e2708d221d6b00bf489e037e9c11075420c7930
IAmQpac <77878877+IAmQpac@users.noreply.github.com>
2022-07-16 17:28:25 -0400

    cleaning code

--- twitterClient.js
+++ twitterClient.js
@@ -18,41 +18,27 @@ const tweet = async ()=>{
         let res = await fetch("https://api.tickertick.com/feed?q=(and (or tt:amzn tt:aapl tt:nflx tt:meta tt:goog tt:msft) (or T:fin_news T:earnings T:market T:sec T:sec_fin))")
 
         let tickerTickJSON = await res.json()
+        let title = tickerTickJSON.stories[0].title
+        let description = tickerTickJSON.stories[0].description
+        let url = tickerTickJSON.stories[0].url
 
+        description.slice(0, 150)
+        description += "..."
 
         let tags;
         let tagString;
+        tags = tickerTickJSON.stories[0].tags
+        for (let i = 0; i < tags.length; i++) {
+            tagString += `$${tags[i].toUpperCase()} `                 
+        }
+
         if(!latestPostedStoryID){
-            let title = tickerTickJSON.stories[0].title
-            let description = tickerTickJSON.stories[0].description
-            let url = tickerTickJSON.stories[0].url
-
-            description.slice(0, 150)
-            description += "..."
-
             console.log("We have a new Story. Posting.. ", title)
-            tags = tickerTickJSON.stories[0].tags
-            for (let i = 0; i < tags.length; i++) {
-                tagString += `$${tags[i].toUpperCase()} `                 
-            }
-
             T.post('statuses/update', {status: `${title}\nDesc: ${description}\n${tagString}\nPowered by TickerTick.com ${url}` }, function (err, data, response) {
                 console.log(data)
             })
             latestPostedStoryID = tickerTickJSON.stories[0].id
         } else if(latestPostedStoryID != tickerTickJSON.stories[0].id){
-            tags = tickerTickJSON.stories[0].tags
-            for (let i = 0; i < tags.length; i++) {
-                tagString += `$${tags[i]} `                 
-            }
-
-            let title = tickerTickJSON.stories[0].title
-            let description = tickerTickJSON.stories[0].description
-            let url = tickerTickJSON.stories[0].url
-
-            description.slice(0, 150)
-            description += "..."
-
             console.log("We have a new Story. Posting.. ", title)
             T.post('statuses/update', {status: `${title}\nDesc: ${description}\n${tagString}\nPowered by TickerTick.com ${url}` }, function (err, data, response) {
                 console.log(data)
