148b91e4d74cc3f712329616f45b191536e4ad98
Christian Jerjian <77878877+Jerjian@users.noreply.github.com>
2024-11-26 03:03:29 -0500

    Delete config.cjs
    Deleted Config File

--- config.cjs
+++
@@ -1,6 +0,0 @@
-module.exports={
-    consumer_key:         "cIXj8JBQbkcjrcZBAPNmoalQ4",
-    consumer_secret:      "ExrfRhOrYPlJ39XJG7In1pVlmiojjx1rWQNNhKS7ZcqpSky5Jp",
-    access_token:         "1547743540343517189-iomDJPawENpESfkYeUGqrJXbIyu3if",
-    access_token_secret:  "dwp7t9GxNG6ofj2lox98maIYP188UWQpYfsygvaP0lHAL"
-}
